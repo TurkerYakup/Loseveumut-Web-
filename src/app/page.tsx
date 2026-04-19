f89dbfed67cd098d04d9f7062b0f7c8116521549
turker altınsoy <altinsoyturkeryakup@gmail.com>
2026-04-20 01:13:51 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-white">
+      <h1 className="text-8xl font-extrabold text-black uppercase">
+        LOSEMİYE UMUT OL </LO>
+      </h1>
+    </div>
+  );
+}
