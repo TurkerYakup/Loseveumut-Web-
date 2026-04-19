1f5a44fc0d950c6db65bb7fee54633c0b56fea07
turker altınsoy <altinsoyturkeryakup@gmail.com>
2026-04-20 01:16:11 +0300

    Update text to LÖSEMYE UMUT OL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-white">
-      <h1 className="text-8xl font-extrabold text-black uppercase">
-        LOSEMİYE UMUT OL </LO>
+      <h1 className="text-8xl font-extrabold text-black uppercase text-center max-w-4xl">
+        LÖSEMİYE UMUT OL
       </h1>
     </div>
   );
